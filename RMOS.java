af5641f3f82e0166bb58ddc5092d539c140f7c13
Kevin Velcich <kvelcich@gmail.com>
2017-03-14 21:44:21 -0700

    Adding intial RMOS

---
+++ RMOS.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class RMOS {
+    ArrayList<RCM> RCMList;
+    ArrayList<Recyclable> recyclablesList;
+
+    public RMOS() {
+        RCMList = new ArrayList<RCM>();
+        recyclablesList = new ArrayList<Recyclable>();
+    }
+
+    public void addRecyclable(Recyclable recyclable) {
+        recyclablesList.add(recyclable);
+    }
+
+}
